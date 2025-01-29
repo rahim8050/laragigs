a530e145bab93516731bc93185cad8472d94b02c
rahim8050 <rahimranxx8050@gmail.com>
2025-01-29 08:07:08 +0000

    ids

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 @section('content')
+@include('partials._hero')
 <div
                 class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
             >
@@ -13,14 +14,14 @@
   <div class="flex">
       <img
           class="hidden w-48 mr-6 md:block"
-          src="images/acme.png"
+          src="{{ asset ('images/no-image.png')}}"
           alt=""
       />
       <div>
           <h3 class="text-2xl">
-              <a href="show.html">{{$listing->title}}</a>
+              <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
           </h3>
-          <div class="text-xl font-bold mb-4">Acme Corp</div>
+          <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
           <ul class="flex">
               <li
                   class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
@@ -44,8 +45,8 @@
               </li>
           </ul>
           <div class="text-lg mt-4">
-              <i class="fa-solid fa-location-dot"></i> Boston,
-              MA
+              <i class="fa-solid fa-location-dot"></i> 
+              {{$listing->location}}
           </div>
       </div>
   </div>
