d11ed1e3a5fd20ec770f354a347d9c87ae7c922f
rahim8050 <rahimranxx8050@gmail.com>
2025-01-29 09:15:03 +0000

    ups

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('listings',[
-        'heading' => 'latest listings',
+    return view('listings', [
+        'heading' => 'Latest Listings',
         'listings' => Listing::all()
     ]);
 });
-Route::get('/listings/{$id}',function($id){
-    return view ('listing',[
-        'listings'=>listing::find($id)
+
+Route::get('/listings/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
     ]);
 });
+
+
 
 
