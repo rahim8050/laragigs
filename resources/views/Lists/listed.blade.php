e2e8e41579af3e147547f5ddc946c6c929eadd3e
rahim8050 <rahimranxx8050@gmail.com>
2025-01-30 14:20:08 +0000

    ins

--- resources/views/Lists/listed.blade.php
+++ resources/views/Lists/listed.blade.php
@@ -8,7 +8,7 @@
         />
         <div>
             <h3 class="text-2xl">
-                <a href="/listings/{{$listing['id']}}">{{$listing->title}}</a>
+                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
             <x-list-tags :tagscsv="$listing->tags" />
