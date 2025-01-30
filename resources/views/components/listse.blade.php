dfc310c3cc6d8e13c5e0a8a2e8749f8b53426ae8
rahim8050 <rahimranxx8050@gmail.com>
2025-01-30 12:29:12 +0000

    lists

--- resources/views/components/listse.blade.php
+++ resources/views/components/listse.blade.php
@@ -9,7 +9,7 @@
         <div>
             <h3 class="text-2xl">
                 <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
-            </h3>
+              </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
             <ul class="flex">
                 <li
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,7 +5,7 @@
                 ><i class="fa-solid fa-arrow-left"></i> Back
             </a>
             <div class="mx-4">
-                <x-card>
+                <x-card class="p-24">
                     <div
                         class="flex flex-col items-center justify-center text-center"
                     >
