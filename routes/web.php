556bc29e289a6b0b7bc13e4b49aae4c3b950853c
rahim8050 <rahimranxx8050@gmail.com>
2025-01-30 16:45:24 +0000

    controllers

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Latest Listings',
         'listings' => Listing::all()
     ]);
 });
